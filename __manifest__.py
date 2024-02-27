f654bd8366976aaa786efc9b213c70b2af6a66db
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-27 17:28:17 -0400

    [CHORE] update the author in the __manifest__.py file

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Module that provides access to creating accounting entries in the account.move model through an HTTP request.',
     'description': 'Module that provides access to creating accounting entries in the account.move model through an HTTP request.',
-    'author': 'Lifter',
+    'author': 'Frainer Encarnación',
     'category': 'Accounting',
     'depends': ['account'],
     'icon': 'account_move_api/static/src/img/icon.png',
